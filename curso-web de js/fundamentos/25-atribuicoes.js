7be95c1e7dcccc934ac99a8ab2caf0220671bcca
ViniFreitasss <vinifreitass20@gmail.com>
2022-01-09 00:55:53 -0300

    atualização de operadores em fundamentos de js

---
+++ curso-web de js/fundamentos/25-atribuicoes.js
@@ -0,0 +1,26 @@
+const a = 7 // colocando o valor 7 dentro da constante a
+
+let b = 2  
+
+b += a  // atribuição aditiva, ou seja, ao acrescentar a em b o valor não será subscrito e sim somado, equivalente a b = b + a
+
+console.log(b)
+
+b -= 4 // atribuição subtrativa, equivalente a b = b - 4
+
+console.log(b)
+
+b *= 2 // b = b * 2
+
+console.log(b)
+
+b /= 2 // b = b / 2
+
+console.log(b)
+
+b %= 2 // b = b % 2
+
+console.log(b)
+
+// sempre o sinal da operação vem primeiro e posteriormente o sinal de atribuição
+
