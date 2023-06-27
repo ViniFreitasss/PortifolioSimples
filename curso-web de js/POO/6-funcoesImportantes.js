bd30a38e32b13c9e4871a89e8cd48c3f3e0f731a
Vinícius de Mello Freitas <vinicius.freitas@BRASTORAGE.LOCAL>
2023-06-27 15:14:09 -0300

    herança

--- curso-web de js/POO/6-funcoesImportantes.js
+++ curso-web de js/POO/6-funcoesImportantes.js
@@ -6,6 +6,8 @@ const pessoa = {
     peso: 13
 }
 
+// funções keys, values e entries
+
 console.log(Object.keys(pessoa))
 console.log(Object.values(pessoa))
 console.log(Object.entries(pessoa))
@@ -14,3 +16,28 @@ Object.entries(pessoa).forEach(e => {
     console.log(`${e[0]}: ${e[1]}`)
 })
 
+// função defineProperty
+
+Object.defineProperty(pessoa, 'dataNascimento', {
+    enumerable: false,
+    writable: false,
+    value: '01/01/2019'
+})
+
+pessoa.dataNascimento = '01/01/2017'
+console.log(pessoa.dataNascimento)
+console.log(Object.keys(pessoa)) 
+
+// Object.assign (ECMAScript 2015)
+// junta atributos de objetos
+const dest = {a: 1}
+const a1 = {b: 1}
+const a2 = {c: 3, a: 4}
+const obj = Object.assign(dest, a1, a2)
+console.log(obj)
+
+
+// Object.freeze
+Object.freeze(obj)
+obj.c = 1234
+console.log(obj)
