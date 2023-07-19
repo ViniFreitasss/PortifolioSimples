3cd6d3e57d03323762cee5706cd1713327cfc53d
Vinícius de Mello Freitas <vinicius.freitas@BRASTORAGE.LOCAL>
2023-07-19 15:30:08 -0300

    Começo do capitulo de node

---
+++ On-curso-web-de-js/node/1-moduloA.js
@@ -0,0 +1,6 @@
+// em node um arquivo representa um módulo
+// existem 2 padrões de módulos: comunsjs e ECMscript 2015
+
+this.ola = 'Fala pessoal' // a frase pode ser acessada em outro arquivo
+exports.bemVindo = 'Bem vido ao noode'
+module.exports.ateLogo = 'Até o próximo exemplo' // forma mais convencional
---
+++ On-curso-web-de-js/node/1-moduloCliente.js
@@ -0,0 +1,8 @@
+const moduloA = require('./1-moduloA')
+const moduloB = require('./1-moduloB')
+
+console.log(moduloA.ola)
+console.log(moduloA.bemVindo)
+console.log(moduloA.ateLogo)
+console.log(moduloB.bomDia)
+console.log(moduloB.boaNoite)
